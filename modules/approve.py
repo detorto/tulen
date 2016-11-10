67e1e0a91b85e7f9dba9d10b5418196ec708c566
John <johnshtefan@gmail.com>
2016-11-11 00:41:42 +0300

    Fixed spacing, gitignore

--- modules/approve.py
+++ modules/approve.py
@@ -9,30 +9,29 @@ import os
 
 CONFIG_FILE = "conf.yaml"
 
+
 class Processor:
+    def __init__(self, vkuser):
+        self.user = vkuser
+        self.config = yaml.load(open(vkuser.module_file("approve", CONFIG_FILE)))
 
-	def __init__(self,vkuser):
-		self.user = vkuser
-		self.config = yaml.load(open(vkuser.module_file("approve", CONFIG_FILE)))
+    def respond(self, word, chatid, userid):
+        reps = [l.strip() for l in open(self.user.module_file("approve", self.config[word]["dict"])).readlines()]
+        rep = random.choice(reps)
+        rand = random.randint(1, 100)
+        if 30 <= rand <= 70:
+            self.user.send_sticker(userid, userid, chatid)
+        self.user.send_message(text=rep, chatid=chatid, userid=userid)
 
-	def respond(self, word, chatid, userid):
-		reps = [l.strip() for l in open(self.user.module_file("approve", self.config[word]["dict"])).readlines()]
-		rep = random.choice(reps)
-		rand = random.randint(1, 100)
-		if 30 <= rand <= 70:
-			self.user.send_sticker(userid, userid, chatid)
-		self.user.send_message(text=rep, chatid=chatid, userid=userid)
+    def process_message(self, message, chatid, userid):
+        if chatid == None:
+            self.respond(random.choice(self.config.keys()), chatid, userid)
 
-	def process_message(self, message, chatid, userid):
+        for word in self.config.keys():
 
-		if chatid == None:
-			self.respond(random.choice(self.config.keys()), chatid, userid)
-		
-		for word in self.config.keys():
-	
-			message_body = message["body"].lower()
+            message_body = message["body"].lower()
 
-			prog = re.compile(word)
+            prog = re.compile(word)
 
-			if prog.match(message_body):
-				self.respond(word, chatid, userid)
+            if prog.match(message_body):
+                self.respond(word, chatid, userid)
