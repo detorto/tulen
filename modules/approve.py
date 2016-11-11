8b0b3bc32aa0d9600c832a2dbdf12231c1eb3a8c
detorto <dpuzyrkov@gmail.com>
2016-11-11 03:24:05 +0300

    only one message in personal chat.

--- modules/approve.py
+++ modules/approve.py
@@ -21,11 +21,20 @@ class Processor:
         rand = random.randint(1, 100)
         if 35 <= rand <= 65:
             self.user.send_sticker(userid, userid, chatid)
-        self.user.send_message(text=rep, chatid=chatid, userid=userid)
+	
+        if rep.startswith("img:"):
+            print rep
+            f = self.user.module_file("approve", rep[rep.rindex("img"):])
+            attc = self.user.upload_images_files([f,])
+            self.user.send_message(text = "", attachments = attc, chatid=chatid, userid=userid)
+            return
+        else:
+            self.user.send_message(text=rep, chatid=chatid, userid=userid)
 
     def process_message(self, message, chatid, userid):
         if chatid == None:
             self.respond(random.choice(self.config.keys()), chatid, userid)
+            return 
 
         for word in self.config.keys():
 
@@ -34,5 +43,7 @@ class Processor:
             prog = re.compile(word)
 
             if prog.match(message_body):
+                print "-------------!!!111!"
                 self.respond(word, chatid, userid)
+                print "--------!!!!"
 
