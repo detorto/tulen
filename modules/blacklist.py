42843b41428e252ed092c72421e1c864ae0b0e0e
detorto <dpuzyrkov@gmail.com>
2017-01-09 23:56:44 +0300

    blacklist improved.

--- modules/blacklist.py
+++ modules/blacklist.py
@@ -9,13 +9,16 @@ import os
 
 CONFIG_FILE = "conf.yaml"
 
+msg = u"""Друге, ты дурак и бан тебе. Если считаешь что не справедливо - иди жалуйся мне на стенку. Иначе - либо донать на каптчу, либо иди нахуй.
+Донаты кидать нa QIWI +79166317941 или яды https://money.yandex.ru/to/41001705503465 от 30 рублей, с пометкой "на разбан" и вашим айдишником. 
+Все деньги пойдут в сервис решения каптчи 2captcha.com, чтобы тюлень всегда был с вами. C уважением, поддержка."""
 
 class Processor:
     def __init__(self, vkuser):
         self.user = vkuser
         self.config = yaml.load(open(vkuser.module_file("blacklist", CONFIG_FILE)))
         self.exclusive = True
-        self.block_list = [u"цп.жпг", u"цп.ави"]
+        self.block_list = [u"цп.жпг", u"цп.ави",u"цп.гиф"]
 
 
     def save(self):
@@ -24,7 +27,7 @@ class Processor:
 
     def block(self, uid):
         self.uids.append(int(uid))
-        self.user.send_message(text=u"Друге, ты дурак и бан тебе. Если считаешь что не справедливо - иди жалуйся мне на стенку. Иначе - либо донать на каптчу, либо иди нахуй. С любовью, Тюлень.", userid=uid)
+        self.user.send_message(text=msg, userid=uid)
         self.save()
 
     def process_message(self, message, chatid, userid):
