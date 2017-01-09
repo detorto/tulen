ea447bf2ee4cc91bc5f5d5c8826441c48df21255
detorto <dpuzyrkov@gmail.com>
2017-01-09 18:32:19 +0300

    fix capchabalance module

--- modules/captcha_balance.py
+++ modules/captcha_balance.py
@@ -14,10 +14,15 @@ class Processor:
 
 	def __init__(self, user):
 		self.user = user
-		self.tcapi  = TwoCaptchaApi("5c1869001bec3af28387c1bc2ba0ab76")
+		capthca_api_key = self.user.config.get("twocaptcha_api_key", None)
+		self.tcapi = None
+		if capthca_api_key:
+			self.tcapi  = TwoCaptchaApi(capthca_api_key)
 
 
 	def process_message(self, message, chatid, userid):
+		if not self.tcapi:
+			return
 		if "captchabalance" in message["body"].lower().encode("utf-8"):
 			balance = self.tcapi.get_balance()
 			self.user.send_message( text=u"Баланс на каптчарешателе: {}$. В день решается около 300 каптч, 1000 каптч стоит 1$. На сколько этого хватит - посчитаете сами.".format(balance), chatid=chatid, userid=userid)
